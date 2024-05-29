f7fcc08af9433157747e502d50e1532efa594079
Andy <ahalterman0@gmail.com>
2024-05-29 08:17:14 -0400

    Update tensors for spaCy 3.7

--- NGEC/utilities.py
+++ NGEC/utilities.py
@@ -8,53 +8,35 @@ import logging
 logger = logging.getLogger(__name__)
 logger.addHandler(logging.NullHandler())
 
-#def make_country_dict():
-#    country = pd.read_csv("assets/wikipedia-iso-country-codes.txt")
-#    country_dict = {i:n for n, i in enumerate(country['Alpha-3 code'].to_list())}
-#    country_dict["CUW"] = len(country_dict)
-#    country_dict["XKX"] = len(country_dict)
-#    country_dict["SCG"] = len(country_dict)
-#    country_dict["SSD"] = len(country_dict)
-#    country_dict["BES"] = len(country_dict)
-#    country_dict["NULL"] = len(country_dict)
-#    country_dict["NA"] = len(country_dict)
-#    return country_dict
-#
-#
-#with open("assets/feature_code_dict.json", "r") as f:
-#    feature_code_dict = json.load(f)
-#
 
 def spacy_doc_setup():
     try:
         Token.set_extension('tensor', default=False)
     except ValueError:
         pass
-
     try:
         @Language.component("token_tensors")
         def token_tensors(doc):
-            chunk_len = len(doc._.trf_data.tensors[0][0])
-            token_tensors = [[]]*len(doc)
-
-            for n, i in enumerate(doc):
-                wordpiece_num = doc._.trf_data.align[n]
-                for d in wordpiece_num.dataXd:
-                    which_chunk = int(np.floor(d[0] / chunk_len))
-                    which_token = d[0] % chunk_len
-                    ## You can uncomment this to see that spaCy tokens are being aligned with the correct 
-                    ## wordpieces.
-                    #wordpiece = doc._.trf_data.wordpieces.strings[which_chunk][which_token]
-                    #print(n, i, wordpiece)
-                    token_tensors[n] = token_tensors[n] + [doc._.trf_data.tensors[0][which_chunk][which_token]]
+            tensors = doc._.trf_data.last_hidden_layer_state
             for n, d in enumerate(doc):
-                if token_tensors[n]:
-                    d._.set('tensor', np.mean(np.vstack(token_tensors[n]), axis=0))
+                if tensors[n]:
+                    d._.set('tensor', tensors[n])
                 else:
-                    d._.set('tensor',  np.zeros(doc._.trf_data.tensors[0].shape[-1]))
+                    d._.set('tensor',  np.zeros(tensors[0].shape[-1]))
             return doc
     except ValueError:
         pass
+
+### TESTING ###
+### Comment this out and run to verify that the new 3.7+ version of spaCy works
+#import spacy
+#nlp = spacy.load("en_core_web_trf")
+#spacy_doc_setup()
+#nlp.add_pipe("token_tensors")
+#
+#doc = nlp("We visited Berlin and Alexanderplatz.")
+#doc[3]._.tensor
+####
 
 def stories_to_events(story_list, doc_list=None):
     if not doc_list:
